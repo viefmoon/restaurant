39e6bd3dc33321dddade1836a131bc6f714851c5
Leonel <viefmoon@gmail.com>
2024-03-25 20:28:35 -0600

    redos

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -16,13 +16,14 @@ export class CategoriesService {
     async findAllWithSubcategoriesAndProducts(): Promise<Category[]> {
         const cacheKey = 'categories_with_details';
         try {
-            // Intentar recuperar de Redis
+            console.log('Intentando recuperar de Redis');
             const cached = await this.redisClient.get(cacheKey);
             if (cached) {
+                console.log('Datos recuperados de Redis');
                 return JSON.parse(cached);
             }
 
-            // Si no está en caché, buscar en la base de datos
+            console.log('Datos no encontrados en Redis, buscando en la base de datos');
             const categories = await this.categoryRepository.find({
                 relations: [
                     'subcategories',
@@ -31,11 +32,12 @@ export class CategoriesService {
                 ],
             });
 
-            // Guardar el resultado en Redis antes de retornar
-            await this.redisClient.set(cacheKey, JSON.stringify(categories), 'EX', 3600); // Expira en 1 hora
+            console.log('Guardando datos en Redis');
+            await this.redisClient.set(cacheKey, JSON.stringify(categories), 'EX', 10); // Expira en 10 segundos
 
             return categories;
         } catch (error) {
+            console.error('Error al recuperar las categorías', error);
             throw new HttpException('Error retrieving categories', HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
